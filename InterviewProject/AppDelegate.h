446fa3e367adc8bd09e837d71ca40c594cb12674
ankur <ankur@ankurs-mac.local>
2016-07-21 00:12:31 +0530

    Initial Commit

---
+++ InterviewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterviewProject
+//
+//  Created by ankur on 21/07/16.
+//  Copyright © 2016 ankur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InterviewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterviewProject
+//
+//  Created by ankur on 21/07/16.
+//  Copyright © 2016 ankur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
